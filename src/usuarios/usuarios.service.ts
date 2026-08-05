2e12744089efc101281928561562dd8901902bca
muinicomuiser <nicodoneg@gmail.com>
2026-08-04 22:23:14 -0400

    Documentación mapeo de array

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -41,16 +41,34 @@ export class UsuariosService {
 
     obtenerTodos(): GetUsuarioDTO[] {
 
-        // const usuariosDto: GetUsuarioDTO[] = this.usuarios.map(usuario => {
-        //     const getDto = new GetUsuarioDTO()
-        //     getDto.id = usuario.id
-        //     getDto.nombre = usuario.nombre
-        //     getDto.direccion = usuario.direccion
-        //     getDto.correoElectronico = usuario.correoElectronico
-        //     return getDto
-        // })
-        const usuariosDto: GetUsuarioDTO[] = this.usuarios.map(usuario => this._toGetDTO(usuario))
-        return usuariosDto
+        // La idea es convertir cada Usuario de la base de datos en un DTO tipo GetUsuarioDTO.
+        // Para eso hay que iterar por cada usuario de la lista y convertirlo.
+        // Tres formas de recorrer el array y aplicar esa transformación
+        // (Para ambas se usará el método definido más abajo "_toGetDTO", que convierte un objeto tipo
+        // Usuario en un objeto DTO tipo GetUsuarioDTO)
+
+        // 1. Con un ciclo for
+        const usuariosDTO_1: GetUsuarioDTO[] = []
+        for(let usuario of this.usuarios){
+            // Reutilizando la función que convierte un Usuario en un DTO
+            const usuarioDTO = this._toGetDTO(usuario)
+            usuariosDTO_1.push(usuarioDTO)
+        }
+
+        // 2. Con el método "forEach()" de array, que ejecuta una acción por cada elemento del arreglo
+        // Se ejecuta la función de transformación a DTO e inserción en el arreglo dentro del argumento
+        // del método .forEach(función)
+        const usuariosDTO_2: GetUsuarioDTO[] = []
+        this.usuarios.forEach(usuario => {
+            const usuarioDTO = this._toGetDTO(usuario)
+            usuariosDTO_2.push(usuarioDTO)            
+        }) 
+
+        // 3. Con el método "map()" de array, que aplica una transformación a cada elemento del arreglo
+        // y retorna un arreglo nuevo con los objetos transformados.
+        // Recibe como parámetro la función de transformación.
+        const usuariosDTO_3: GetUsuarioDTO[] = this.usuarios.map(usuario => this._toGetDTO(usuario))
+        return usuariosDTO_3
     }
 
     eliminarPorID(id: number){
